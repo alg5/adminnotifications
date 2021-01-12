bdcab33e048f43bd5d4779b90c1c6aa303c1f7dc
Alg <chgk.globus@gmail.com>
2021-01-12 20:57:59 +0200

    TWIG sintax

--- event/listener.php
+++ event/listener.php
@@ -17,21 +17,10 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class listener implements EventSubscriberInterface
 {
 
-	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user, $phpbb_root_path, $php_ext, \phpbb\auth\auth $auth)
-	{
-		$this->template = $template;
-		$this->user = $user;
-		$this->config = $config;
-		$this->phpbb_root_path = $phpbb_root_path;
-		$this->php_ext = $php_ext;
-		$this->auth = $auth;
-
-	}
-
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.user_setup'					=>	'load_language_on_setup',
+						'core.user_setup'   =>  'load_language_on_setup',
 		);
 	}
 	public function load_language_on_setup($event)
